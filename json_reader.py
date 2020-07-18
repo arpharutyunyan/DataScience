b20091ca10b6e5cc9772a6aa376a76fc494ffb5f
Arpine <arpine.harutyunyana@mail.ru>
2020-07-18 12:41:16 +0400

    creating json file

--- json_reader.py
+++ json_reader.py
@@ -8,9 +8,3 @@ reader=req.json()
 res=json.dumps(reader)
 with open("file.json", "w") as f:
     f.write(res)
-# df=pd.DataFrame(res)
-
-# reader = pd.read_json(link)
-# writer = df.to_json("file.json")
-# print(res)
-# print(df.head())
