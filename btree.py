58777168be950f9b70163a5e937edb88b495a233
Arpine <arpine.harutyunyana@mail.ru>
2020-07-28 20:49:52 +0400

    sorted and search

--- btree.py
+++ btree.py
@@ -1,13 +1,12 @@
 import random
 
 def main():
-    
+    num=int(input('Which number do you search: '))
     # create list with numbers
     list_nums=random.choices([i for i in range(1000)], k=500)
     # sort lists
     sort_list=sorted_list(list_nums)
-    num=int(input('Which number do you search: '))
-    # print(sort_list)
+    # find number
     find_num_btree(num, sort_list)
 
 
@@ -15,20 +14,28 @@ def sorted_list(lists):
     # check if list is sorted
     if lists==lists.sort():
         return 
+    # if list is not sorted 
     lists.sort()
     return lists
 
 
 def find_num_btree(number, lists):
-    l=0
-    r=len(lists)-1
-    while r-l>1:
-        m=(l+r)//2
-        if number<lists[m]:
-            r=m-1
+    # left border
+    left=0
+    # right border
+    right=len(lists)-1
+    while right-left>1:
+        # middel is half of list's lengh 
+        middel=(left+right)//2
+        # if number is smaller than middel number from list
+        if number<lists[middel]:
+            # right border is middel index
+            right=middel-1
         else:
-            l=m
-    if number==lists[l] or number==lists[r]:
+            # left border will be middel index
+            left=middel
+    # check two borders, because <while> condition >1
+    if number==lists[left] or number==lists[right]:
         print(f'Your number {number} exists.')
     else:
         print(f'Your number {number} doesn\'t exist.')
